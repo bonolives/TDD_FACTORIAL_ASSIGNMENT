e36b99ff81de83249ef2356946cfb35693c50004
olive <bon_olives@yahoo.com>
2023-06-27 00:51:07 +0300

    DEFINE FACTORIAL FUNCTION ONE USING LOOP METHOD

--- factorial.py
+++ factorial.py
@@ -2,3 +2,13 @@
 
 # writing the factorial function
 
+# Defining the function using Iterative approach ( using the 'loop' method)
+
+def factorial_one(var_1):
+    answer = 1
+    for i in range(1, var_1 + 1):
+        answer = answer * i
+    return answer 
+
+
+
