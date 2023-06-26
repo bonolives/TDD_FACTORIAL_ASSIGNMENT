2ea5eb02768312dc6f57d1a1cc61ad282d06e3f8
olive <bon_olives@yahoo.com>
2023-06-27 01:53:40 +0300

    ADD TEST TWO FAILED

--- factorial.py
+++ factorial.py
@@ -4,18 +4,19 @@
 
 # Defining the function using Iterative approach ( using the 'loop' method)
 
+
 def factorial_one(var_1):
+    """function"""
     answer = 1
     for i in range(1, var_1 + 1):
         answer = answer * i
-    return answer 
+    return answer
+
 
 # Defining function using recursion approach.
 def factorial_two(var_2):
+    """function"""
     if var_2 == 0:
-       return 1
+        return 1
     else:
         return var_2 * factorial_two(var_2 - 1)
-    
-
-
--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 # Here we import the function from factorial.py file
 
-from factorial import factorial_one
+from factorial import factorial_one, factorial_two
 
 # defining a test function to test the loop method ( factorial_one)
 
-'''
+"""
 def test_one():
     assert factorial_one(15) == 159
-'''
+
 def test_one():
     assert factorial_one(15) == 1307674368000
-    
+"""
+
+# defining a test function to test the recursion approach.
+
+
+def test_two():
+    assert factorial_two(5) == 115
